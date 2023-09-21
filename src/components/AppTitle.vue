7447256ff78e35cf1d2aedbbad5560e7f54a70a0
Valentina De Mite <valesakura97@gmail.com>
2023-09-21 20:55:51 +0200

    completed exercise and added bonus

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script></script>
+
+<template>
+  <div class="Title">
+    <h1>La mia prima App con VITE!</h1>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script></script>
+
+<template>
+  <div class="card">
+    <p>Hello World!</p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
